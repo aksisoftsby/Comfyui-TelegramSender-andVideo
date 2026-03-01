d3f9a94af41809019a797b69006a97665e9f5137
aksigit <30791021+aksisoftsby@users.noreply.github.com>
2026-03-01 13:07:00 +0700

    Add TelegramSenderVideo to mappings

--- __init__.py
+++ __init__.py
@@ -1,6 +1,14 @@
 from .telegram_sender import TelegramSender
+from .telegram_sender_video import TelegramSenderVideo
 
-NODE_CLASS_MAPPINGS = {"TelegramSender": TelegramSender}
-NODE_DISPLAY_NAME_MAPPINGS = {"TelegramSender": "Telegram Sender"}
+NODE_CLASS_MAPPINGS = {
+    "TelegramSender": TelegramSender,
+    "TelegramSenderVideo": TelegramSenderVideo,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "TelegramSender": "Telegram Sender",
+    "TelegramSenderVideo": "Telegram Sender Video",
+}
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
