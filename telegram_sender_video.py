aca24dd4ed7b1d65063fb1671d5b17bc95f7b8fc
aksigit <30791021+aksisoftsby@users.noreply.github.com>
2026-03-01 13:09:49 +0700

    Add TelegramSenderVideo class to send videos

---
+++ telegram_sender_video.py
@@ -0,0 +1,27 @@
+class TelegramSenderVideo:
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "video": ("VIDEO",),
+                "chat_id": ("STRING", {"default": ""}),
+                "bot_token": ("STRING", {"default": ""}),
+            }
+        }
+
+    RETURN_TYPES = ()
+    FUNCTION = "send_video"
+    CATEGORY = "Telegram"
+
+    def send_video(self, video, chat_id, bot_token):
+
+        video_path = video  # biasanya sudah berupa path file
+
+        with open(video_path, "rb") as f:
+            url = f"https://api.telegram.org/bot{bot_token}/sendVideo"
+            files = {"video": f}
+            data = {"chat_id": chat_id}
+            requests.post(url, data=data, files=files)
+
+        return ()
