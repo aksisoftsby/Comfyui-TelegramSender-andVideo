25c23b1b92892f26c26b305eaab8fdf9f2131eaf
aksigit <30791021+aksisoftsby@users.noreply.github.com>
2026-03-02 07:12:38 +0700

    runall

---
+++ run_all.py
@@ -0,0 +1,12 @@
+import subprocess
+
+# Start watcher
+watcher = subprocess.Popen(["python", "watcher-modal.py"])
+watcher2 = subprocess.Popen(["python", "watcher-collab.py"])
+
+# Start ComfyUI
+comfy = subprocess.Popen(["python", "main.py", "--dont-print-server"])
+
+watcher.wait()
+watcher2.wait()
+comfy.wait()
