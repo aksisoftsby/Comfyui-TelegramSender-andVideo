31eec862ddac5e20a031c6cdbe8ac9ff78413d3e
aksigit <30791021+aksisoftsby@users.noreply.github.com>
2026-03-09 00:27:18 +0700

    Fix formatting and add newline at end of run_all.py
    Fix missing newline at end of file and clean up formatting.

--- run_all.py
+++ run_all.py
@@ -5,8 +5,9 @@ watcher = subprocess.Popen(["python", "watcher-modal.py"])
 watcher2 = subprocess.Popen(["python", "watcher-collab.py"])
 
 # Start ComfyUI
-comfy = subprocess.Popen(["python", "main.py", "--dont-print-server", "--novram"])
+comfy = subprocess.Popen(["python", "main.py", "--dont-print-server"])
 
 watcher.wait()
 watcher2.wait()
+
 comfy.wait()
