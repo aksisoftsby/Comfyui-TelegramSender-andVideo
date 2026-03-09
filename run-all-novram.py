42a71a96cf22b7c67449e9f1c6f6f591040e2cd9
aksigit <30791021+aksisoftsby@users.noreply.github.com>
2026-03-10 04:48:04 +0700

    Add run-all-novram.py to start watchers and ComfyUI

---
+++ run-all-novram.py
@@ -0,0 +1,13 @@
+import subprocess
+
+# Start watcher
+watcher = subprocess.Popen(["python", "watcher-modal.py"])
+watcher2 = subprocess.Popen(["python", "watcher-collab.py"])
+
+# Start ComfyUI
+comfy = subprocess.Popen(["python", "main.py", "--dont-print-server", "--novram"])
+
+watcher.wait()
+watcher2.wait()
+
+comfy.wait()
